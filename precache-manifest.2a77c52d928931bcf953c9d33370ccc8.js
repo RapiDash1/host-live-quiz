c4c16ba7570c086ff2d506b7dc39354ec808081b
RapiDash <srivatsa.m11@gmail.com>
2020-09-24 22:24:00 +0530

    Updates

---
+++ precache-manifest.2a77c52d928931bcf953c9d33370ccc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1f1bdd16614185af2cdb1a02d7c2ca",
+    "url": "/live-quiz/index.html"
+  },
+  {
+    "revision": "ae004dec5806bbbcce04",
+    "url": "/live-quiz/static/css/main.0087de97.chunk.css"
+  },
+  {
+    "revision": "2938d748adf69cfee322",
+    "url": "/live-quiz/static/js/2.f86ad6ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live-quiz/static/js/2.f86ad6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae004dec5806bbbcce04",
+    "url": "/live-quiz/static/js/main.c3edcf8d.chunk.js"
+  },
+  {
+    "revision": "329e31424f35108b99c8",
+    "url": "/live-quiz/static/js/runtime-main.7bfa9b07.js"
+  }
+]);
